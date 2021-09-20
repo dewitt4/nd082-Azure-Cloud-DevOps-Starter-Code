d496bf45873ae206af23c7389476df997d9558c9
DeWitt Gibson <dewittgibson@gmail.com>
2021-09-20 16:09:45 -0700

    update: custom image variables
    add variable names that will be needed to use Packer custom images in Terraform main.tf file

--- C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -1,3 +1,11 @@
 variable "cust_scope" {
     default = "{scope}"
 }
+
+# Custom Images Created using Packer 
+variable "managed_image_name" {
+   description = "Name of the managed image"
+}
+variable "managed_image_resource_group_name" {
+   description = "Name of the managed image resource group"
+}
