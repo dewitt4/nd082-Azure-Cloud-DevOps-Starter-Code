013c50517f339e24797bf898bb1fa89640ed522c
DeWitt Gibson <dewittgibson@gmail.com>
2021-09-20 15:49:44 -0700

    update: network interface
    Add example code for network interface

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -40,6 +40,17 @@ resource "azurerm_network_security_group" "example" {
 }
 
 # Create a Network Interface 
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
 # Create a Public IP
 
