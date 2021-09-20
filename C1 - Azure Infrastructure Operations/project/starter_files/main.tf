eb4fa5ce6b95b93861c64333764c7d593e5b765f
DeWitt Gibson <dewittgibson@gmail.com>
2021-09-20 16:07:53 -0700

    update: create custom VMs with Packer
    add Virtual Machines using the custom images created by Packer - names found in variables.tf file

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -88,6 +88,11 @@ resource "azurerm_availability_set" "example" {
 }
 
 # Create Virtual Machines 
+# Packer creates the custom image - use this to create VM
+data "azurerm_image" "customimage" {
+   name                = var.managed_image_name
+   resource_group_name = var.managed_image_resource_group_name
+}
 
 # Create Managed Disks for the Virtual Machines 
 
