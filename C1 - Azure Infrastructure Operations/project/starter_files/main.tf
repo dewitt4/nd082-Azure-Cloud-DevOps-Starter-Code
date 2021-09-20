dcdd8b559ef35d5ecc217b5f23bcd5cd84be5e5b
DeWitt Gibson <dewittgibson@gmail.com>
2021-09-20 15:53:51 -0700

    update: load balancer
    Add load balancer to Public IP Address

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -65,6 +65,16 @@ resource "azurerm_public_ip" "example" {
 }
 
 # Create a Load Balancer 
+resource "azurerm_lb" "example" {
+  name                = "TestLoadBalancer"
+  location            = "West US"
+  resource_group_name = azurerm_resource_group.example.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
 
 # Create a Virtual Machine Availability Set
 
