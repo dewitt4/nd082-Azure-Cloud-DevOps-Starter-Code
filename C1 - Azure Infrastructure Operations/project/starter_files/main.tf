da2cf9477f758570f59de41a576ed74ae020f310
DeWitt Gibson <dewittgibson@gmail.com>
2021-09-20 15:44:52 -0700

    update: Network Security Group
    Add example code for Network Security Group

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -28,6 +28,11 @@ resource "azurerm_virtual_network" "example" {
 }
 
 # Create a Network Security Group 
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
 # Create a Network Interface 
 
