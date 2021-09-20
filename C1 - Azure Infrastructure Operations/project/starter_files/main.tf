83d96023a7287cbd875b04ab32f902f7d4a69024
DeWitt Gibson <dewittgibson@gmail.com>
2021-09-20 15:47:44 -0700

    update: virtual network and subnet
    add example code for network and subnet

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -21,11 +21,16 @@ resource "azurerm_resource_group" "example" {
 
 # Create a virtual network and a subnet within the resource group
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
+  name                = "virtualNetwork1"
   location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   address_space       = ["10.0.0.0/16"]
-}
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
 
 # Create a Network Security Group 
 resource "azurerm_network_security_group" "example" {
